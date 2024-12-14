c5c6ec3669d356cd6f5f606495c00c8bfc5ddd82
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-12-15 01:10:52 +0530

    modified db config

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 
 export const connectToDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL);
-        console.log('Connected to DB');
+        await mongoose.connect(`${process.env.DB_URL} + ${process.env.DB_NAME}`);
+    console.log(`Connected to Database ==> ${process.env.DB_NAME}`);
+
     } catch (error) {
         console.error('Error connecting to database:', error.message);
     }
